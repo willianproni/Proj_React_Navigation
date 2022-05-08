e4c7c3da3c8d3fedd477040d9e10d37f0b67bfc1
willianproni <wifepro@hotmail.com>
2022-05-08 11:07:10 -0300

    Tela de login e Register

--- ReactNavigationProj/src/routes/Stack/index.tsx
+++ ReactNavigationProj/src/routes/Stack/index.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { Home } from "../../screens/Home"
 import { Profile } from "../../screens/Profile"
 import { Search } from "../../screens/Search"
+import { Register } from "../../screens/Register";
 import { propsNavigationStack } from "./Models";
 
 const { Navigator, Screen } = createStackNavigator<propsNavigationStack>();
@@ -11,9 +12,24 @@ const { Navigator, Screen } = createStackNavigator<propsNavigationStack>();
 export default function() {
     return(
         <Navigator initialRouteName="Home">
-            <Screen name="Home" component={Home} />
-            <Screen name="Profile" component={Profile} />
+            <Screen name="Home" component={Home} options={{
+                title: "Login",
+                headerTitleAlign: "center",
+                headerStyle:{
+                    backgroundColor: "#404040",
+                    height: 100,
+                },
+                headerTitleStyle:{
+                    color: "#fff",
+                    fontSize: 30,
+                    fontWeight: 'bold'
+                }
+            }} />
+            <Screen name="Profile" component={Profile} options={(props) =>({
+                title: props.route.params.name
+            })}/>
             <Screen name="Search" component={Search} />
+            <Screen name="Register" component={Register}></Screen>
         </Navigator>
     );
 }
---
+++ ReactNavigationProj/src/screens/Register/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native"
+
+export const Container = styled.SafeAreaView`
+    flex: 1;
+`;
+
+export const RegisterInput = styled.TextInput`
+    background: #ddd;
+    margin-top: 12px;
+    border-radius: 10px;
+    padding: 10px;
+`;
